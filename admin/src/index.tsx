adf2bfc6e175d1d4487e4eae73e45932068f3c0d
Issi Mac <issi.dev.iobroker@gmail.com>
2021-10-17 19:48:13 +0200

    Initial commit

---
+++ admin/src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '@iobroker/adapter-react/Theme';
+import Utils from '@iobroker/adapter-react/Components/Utils';
+import App from './app';
+
+let themeName = Utils.getThemeName();
+
+function build(): void {
+	ReactDOM.render(
+		<ThemeProvider theme={theme(themeName)}>
+			<App
+				adapterName="fully_react_test"
+				onThemeChange={(_theme) => {
+					themeName = _theme;
+					build();
+				}}
+			/>
+		</ThemeProvider>,
+		document.getElementById('root'),
+	);
+}
+
+build();
