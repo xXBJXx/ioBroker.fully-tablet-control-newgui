243f4623ca3688756a5e8eef021534cdb9c70a94
Issi <issi.dev.iobroker@gmail.com>
2022-01-02 14:58:29 +0100

    prettier config customized

--- .prettierrc.js
+++ .prettierrc.js
@@ -6,4 +6,15 @@ module.exports = {
 	useTabs: true,
 	tabWidth: 4,
 	endOfLine: 'lf',
+
+	overrides: [
+		{
+			files: 'admin/src/i18n/*.json',
+			options: {
+				useTabs: false,
+			},
+		},
+	],
+
+	plugins: [require('prettier-plugin-organize-imports')],
 };
